e8c0f0b8b108c1fd6406f18738208f0d5d093dd3
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-03 22:24:30 +0530

    feat: to handle

---
+++ src/main/java/com/swg/configuration/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.swg.configuration;
+
+public class JwtConstant  {
+    public static final String JWT_SECRET = "yadsfwagfjshjhytvyu,jdguy.uysriweui;iruyuy'iuuyryr.teqwruqrpurew";
+    public static final String JWT_HEADER = "Authorization";
+}
