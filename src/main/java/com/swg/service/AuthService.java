4a97a2f8536b9ce99b0838c241d1b3bb544bfb68
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-08 21:52:36 +0530

    feat: create AuthService interface for user authentication methods

---
+++ src/main/java/com/swg/service/AuthService.java
@@ -0,0 +1,14 @@
+package com.swg.service;
+
+import com.swg.exceptions.UserException;
+import com.swg.payload.dto.UserDto;
+import com.swg.payload.response.AuthResponse;
+
+public interface AuthService {
+
+
+    AuthResponse signup(UserDto userDto) throws UserException;
+    AuthResponse login(UserDto userDto);
+
+
+}
