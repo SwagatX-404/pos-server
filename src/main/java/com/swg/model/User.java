117cf3addc807f3d84ac78eb0fc2b30825920a12
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-10 23:10:53 +0530

    fix: add default auto generator unique id for 'User' during signup

--- src/main/java/com/swg/model/User.java
+++ src/main/java/com/swg/model/User.java
@@ -2,9 +2,7 @@ package com.swg.model;
 
 
 import com.swg.domain.UserRole;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.*;
 
@@ -19,6 +17,7 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
