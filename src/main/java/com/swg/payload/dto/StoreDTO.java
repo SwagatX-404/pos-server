f2c51bd8f1b5b6cc93a0b64aa122dde5eebb6182
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-17 22:13:27 +0530

    feat: add StoreDTO class for transferring store data

---
+++ src/main/java/com/swg/payload/dto/StoreDTO.java
@@ -0,0 +1,31 @@
+package com.swg.payload.dto;
+
+import com.swg.domain.StoreStatus;
+import com.swg.model.StoreContact;
+import com.swg.model.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+public class StoreDTO {
+
+
+    private Long id;
+
+    private  String brand;
+
+    private UserDto storeAdmin;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private String description;
+
+    private  String storeType;
+
+    private StoreStatus status;
+
+    private StoreContact contact;
+
+
+}
