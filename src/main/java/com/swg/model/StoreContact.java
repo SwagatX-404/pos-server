609d10724be04f13470ee1bccee04d1a87121f80
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-17 22:13:07 +0530

    feat: add StoreContact class for managing store contact information

---
+++ src/main/java/com/swg/model/StoreContact.java
@@ -0,0 +1,20 @@
+package com.swg.model;
+
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class StoreContact {
+
+    private String address;
+    private String phone;
+
+    @Email(message = "invalid email formate...!")
+    private String email;
+
+
+
+}
