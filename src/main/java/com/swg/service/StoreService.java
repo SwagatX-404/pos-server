f179161aadcc6cbd53c6b4d37c8a556b34bd2f06
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-19 21:07:28 +0530

    feat: expand StoreService interface with additional store management methods

--- src/main/java/com/swg/service/StoreService.java
+++ src/main/java/com/swg/service/StoreService.java
@@ -1,10 +1,20 @@
 package com.swg.service;
 
+import com.swg.model.Store;
 import com.swg.model.User;
 import com.swg.payload.dto.StoreDTO;
+
+import java.util.List;
 
 public interface StoreService {
 
     StoreDTO createStore(StoreDTO storeDTO, User user);
+    StoreDTO getStoreById(Long id);
+    List<StoreDTO> getAllStores();
+    Store getStoreByAdmin();
+    StoreDTO updateStore(Long id, StoreDTO storeDTO);
+    StoreDTO deleteStore(Long id);
+    StoreDTO getStoreByEmployee();
+
 
 }
