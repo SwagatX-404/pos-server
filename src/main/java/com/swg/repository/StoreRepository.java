36704628248718d5a451083a76651509ae7e052d
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-20 22:08:39 +0530

    feat: create StoreRepository interface for store data access

---
+++ src/main/java/com/swg/repository/StoreRepository.java
@@ -0,0 +1,9 @@
+package com.swg.repository;
+
+import com.swg.model.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    Store findByStoreAdminId(Long adminId);
+}
