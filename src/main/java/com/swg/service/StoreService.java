3710f189de95f9baaa31e291bf82fbaff1fbcfe5
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-22 22:33:47 +0530

    feat: update StoreService interface to include exception handling for store operations

--- src/main/java/com/swg/service/StoreService.java
+++ src/main/java/com/swg/service/StoreService.java
@@ -1,5 +1,6 @@
 package com.swg.service;
 
+import com.swg.exceptions.UserException;
 import com.swg.model.Store;
 import com.swg.model.User;
 import com.swg.payload.dto.StoreDTO;
@@ -11,10 +12,10 @@ public interface StoreService {
     StoreDTO createStore(StoreDTO storeDTO, User user);
     StoreDTO getStoreById(Long id) throws Exception;
     List<StoreDTO> getAllStores();
-    Store getStoreByAdmin();
-    StoreDTO updateStore(Long id, StoreDTO storeDTO);
-    StoreDTO deleteStore(Long id);
-    StoreDTO getStoreByEmployee();
+    Store getStoreByAdmin() throws UserException;
+    StoreDTO updateStore(Long id, StoreDTO storeDTO) throws Exception;
+    void deleteStore(Long id) throws UserException;
+    StoreDTO getStoreByEmployee() throws UserException;
 
 
 }
