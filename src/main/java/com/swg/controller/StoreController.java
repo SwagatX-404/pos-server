d835d3d16f9a650187eeaa3a070fcf796ebbbe4a
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-23 22:09:02 +0530

    feat: expand StoreController with additional endpoints for retrieving and managing stores

--- src/main/java/com/swg/controller/StoreController.java
+++ src/main/java/com/swg/controller/StoreController.java
@@ -2,13 +2,17 @@ package com.swg.controller;
 
 
 import com.swg.exceptions.UserException;
+import com.swg.mapper.StoreMapper;
 import com.swg.model.User;
 import com.swg.payload.dto.StoreDTO;
+import com.swg.payload.response.ApiResponse;
 import com.swg.service.StoreService;
 import com.swg.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,4 +40,52 @@ public class StoreController {
 
         return ResponseEntity.ok(storeService.getStoreById(id));
     }
+
+
+    @GetMapping()
+    public ResponseEntity<List<StoreDTO>> getAllStore(
+
+            @RequestHeader ("Authorization") String jwt) throws Exception {
+
+        return ResponseEntity.ok(storeService.getAllStores());
+    }
+
+
+    @GetMapping("/admin")
+    public ResponseEntity<StoreDTO> getStoreByAdmin(
+
+            @RequestHeader ("Authorization") String jwt) throws Exception {
+
+        return ResponseEntity.ok(StoreMapper.toDTO(storeService.getStoreByAdmin()));
+    }
+
+
+    @GetMapping("/employee")
+    public ResponseEntity<StoreDTO> getStoreByEmployee(
+
+            @RequestHeader ("Authorization") String jwt) throws Exception {
+
+        return ResponseEntity.ok(storeService.getStoreByEmployee());
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StoreDTO> updateStore(
+            @PathVariable Long id,
+            @RequestBody StoreDTO storeDTO ) throws Exception {
+
+        return ResponseEntity.ok(storeService.updateStore(id, storeDTO));
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteStore(
+            @PathVariable Long id ) throws Exception {
+
+        storeService.deleteStore(id);
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setMessage("Store deleted successfully..!");
+        return ResponseEntity.ok(apiResponse);
+    }
+
 }
