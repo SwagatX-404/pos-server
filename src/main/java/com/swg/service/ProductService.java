34a31785002136ce224cbb4fe5f694e996088066
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-28 02:04:31 +0530

    feat: create ProductService interface with CRUD methods for product management

---
+++ src/main/java/com/swg/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.swg.service;
+
+import com.swg.model.User;
+import com.swg.payload.dto.ProductDTO;
+
+import java.util.List;
+
+public interface ProductService {
+
+
+    ProductDTO createProduct(ProductDTO productDTO, User user);
+    ProductDTO updateProduct(Long Id, ProductDTO ProductDTO, User user);
+    void deleteProduct(Long id, User user);
+    List<ProductDTO> getProductsByStoreId(Long storeId);
+    List<ProductDTO> searchByKeyword(Long storeId, String keyword);
+
+    }
+
