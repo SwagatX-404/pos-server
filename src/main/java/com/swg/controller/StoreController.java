1eb82695498c99b61c25155375ce01c9727e197f
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-27 22:20:06 +0530

    feat: add moderateStore method and update store retrieval in StoreController

--- src/main/java/com/swg/controller/StoreController.java
+++ src/main/java/com/swg/controller/StoreController.java
@@ -1,6 +1,7 @@
 package com.swg.controller;
 
 
+import com.swg.domain.StoreStatus;
 import com.swg.exceptions.UserException;
 import com.swg.mapper.StoreMapper;
 import com.swg.model.User;
@@ -10,13 +11,20 @@ import com.swg.service.StoreService;
 import com.swg.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
+//Update store api is not working , when i try to update is showing exception "user not found...!"
+
+
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/store")
+@RequestMapping("/api/stores")
 public class StoreController {
 
 
@@ -33,13 +41,7 @@ public class StoreController {
     }
 
 
-    @GetMapping("/{id}")
-    public ResponseEntity<StoreDTO> getStoreById(
-            @PathVariable Long id,
-            @RequestHeader ("Authorization") String jwt) throws Exception {
 
-        return ResponseEntity.ok(storeService.getStoreById(id));
-    }
 
 
     @GetMapping()
@@ -70,6 +72,7 @@ public class StoreController {
 
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_STORE_ADMIN')")
     public ResponseEntity<StoreDTO> updateStore(
             @PathVariable Long id,
             @RequestBody StoreDTO storeDTO ) throws Exception {
@@ -78,7 +81,27 @@ public class StoreController {
     }
 
 
-    @PutMapping("/{id}")
+
+    @PutMapping("/{id}/moderate")
+    public ResponseEntity<StoreDTO> moderateStore(
+            @PathVariable Long id,
+            @RequestParam StoreStatus status
+    ) throws Exception {
+
+        return ResponseEntity.ok(storeService.moderateStore(id, status));
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StoreDTO> getStoreById(
+            @PathVariable Long id,
+            @RequestHeader ("Authorization") String jwt) throws Exception {
+
+        return ResponseEntity.ok(storeService.getStoreById(id));
+    }
+
+
+    @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse> deleteStore(
             @PathVariable Long id ) throws Exception {
 
