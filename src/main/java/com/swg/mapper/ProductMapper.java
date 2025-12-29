26c78ed9e4501747744b6139c117d889d9caba2f
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-29 22:21:37 +0530

    Refactor: update product mapping logic

--- src/main/java/com/swg/mapper/ProductMapper.java
+++ src/main/java/com/swg/mapper/ProductMapper.java
@@ -6,7 +6,7 @@ import com.swg.payload.dto.ProductDTO;
 
 public class ProductMapper {
 
-    public ProductDTO toDTO(Product product){
+    public static ProductDTO toDTO(Product product){
 
         return ProductDTO.builder()
                 .id(product.getId())
@@ -25,7 +25,7 @@ public class ProductMapper {
 
     }
 
-    public Product toEntity(ProductDTO productDTO, Store store){
+    public static Product toEntity(ProductDTO productDTO, Store store){
 
        return Product.builder()
                 .name(productDTO.getName())
