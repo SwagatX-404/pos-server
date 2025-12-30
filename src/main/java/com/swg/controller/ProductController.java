a49fcc3eaf765b53b906da32c9698b8de3dee4ae
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-30 21:56:17 +0530

    Feat: implement product functionality in ProductController

--- src/main/java/com/swg/controller/ProductController.java
+++ src/main/java/com/swg/controller/ProductController.java
@@ -1,4 +1,71 @@
 package com.swg.controller;
 
+
+import com.swg.model.User;
+import com.swg.payload.dto.ProductDTO;
+import com.swg.payload.response.ApiResponse;
+import com.swg.service.ProductService;
+import com.swg.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
+
+    private  final ProductService productService;
+    private final UserService userService;
+
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> create(
+            @RequestBody ProductDTO productDTO,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.getUserFromJwtToken(jwt);
+        //Throw exception to createProduct(productDTO, null)
+        return ResponseEntity.ok(productService.createProduct(productDTO, user));
+    }
+
+
+
+    @GetMapping("/store/{storeId}")
+    public ResponseEntity<List<ProductDTO>> getByStoreId(
+            @PathVariable Long storeId,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
+        return ResponseEntity.ok(productService.getProductsByStoreId(storeId));
+    }
+
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProductDTO> update(
+            @PathVariable Long id,
+            @RequestBody ProductDTO productDTO,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.getUserFromJwtToken(jwt);
+        return ResponseEntity.ok(productService.updateProduct(id, productDTO, user));
+    }
+
+
+    //storeId
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> delete(
+            @PathVariable Long id,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.getUserFromJwtToken(jwt);
+        productService.deleteProduct(id, user);
+
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setMessage("Product deleted successfully");
+        return ResponseEntity. ok(apiResponse);
+    }
 }
