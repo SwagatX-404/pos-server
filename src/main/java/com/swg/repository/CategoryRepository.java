7bd0aca5748552aa41f77642a8321b39b4a74e74
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-31 21:50:26 +0530

    Reapply "Feat: add getmapping to search in productcontroller"
    This reverts commit 50d8dcb4ca3e39a7c7b7e22f39d4cf561c362c8a.

---
+++ src/main/java/com/swg/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.swg.repository;
+
+import com.swg.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> FindByStoreId(long storeId);
+}
