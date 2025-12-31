b8a66a85cf0ee1d90106f6eddf2e1e9e06ce703c
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-12-31 22:39:54 +0530

    Refactor: add comment for clarity in CategoryRepository

--- src/main/java/com/swg/repository/CategoryRepository.java
+++ src/main/java/com/swg/repository/CategoryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
+//Repository interface for Category entity(Category Repository)
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     List<Category> FindByStoreId(long storeId);
