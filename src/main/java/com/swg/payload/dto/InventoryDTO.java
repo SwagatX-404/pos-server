07cb65d26eef60a178b6bdff518146c47abdef4b
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-10 09:54:56 +0530

    fix: set compolsory @NoArgsConstructor @AllArgsConstructor

--- src/main/java/com/swg/payload/dto/InventoryDTO.java
+++ src/main/java/com/swg/payload/dto/InventoryDTO.java
@@ -4,13 +4,17 @@ import com.swg.model.Branch;
 import com.swg.model.Product;
 import jakarta.persistence.Column;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class InventoryDTO {
 
     private Long id;
