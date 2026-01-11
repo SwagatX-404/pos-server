23780523f109141617fdfb6aaa925254c21363ef
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-11 19:57:45 +0530

    feat: include repository findByStore and findByBranchId

--- src/main/java/com/swg/repository/UserRepository.java
+++ src/main/java/com/swg/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package com.swg.repository;
 
+import com.swg.model.Store;
 import com.swg.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+
+    List<User> findByStore(Store store);
+
+    List<User> findByBranchId(Long branchId);
 }
