0c59141a7146b9bcb29ef2528edf5e45eabe8b1c
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-13 21:44:26 +0530

    fix: .map( usermapper)

--- src/main/java/com/swg/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/swg/service/impl/EmployeeServiceImpl.java
@@ -109,22 +109,22 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 
     @Override
-    public List<User> findStoreEmployees(Long storeId, UserRole role) throws Exception {
+    public List<UserDto> findStoreEmployees(Long storeId, UserRole role) throws Exception {
 
         Store store = storeRepository.findById(storeId)
                 .orElseThrow(
                         () -> new Exception("Store not found...!")
                 );
-        return userRepository.findByStore(store).
-                stream().filter(
-                        user -> role==null || user.getRole()==role
-                ).collect(Collectors.toList());
+        return userRepository.findByStore(store)
+                .stream().filter(user -> role==null || user.getRole()==role)
+                .map(UserMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
 
 
     @Override
-    public List<User> findBranchEmployees(Long branchId, UserRole role) throws Exception {
+    public List<UserDto> findBranchEmployees(Long branchId, UserRole role) throws Exception {
 
         Branch branch = branchRepository.findById(branchId).orElseThrow(
                 () -> new Exception("Branch not found...!")
@@ -132,8 +132,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 
         return userRepository.findByBranchId(branchId)
-                .stream().filter(
-                        user -> role==null || user.getRole()==role
-                ).collect(Collectors.toList());
+                .stream().filter(user -> role==null || user.getRole()==role)
+                .map(UserMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
