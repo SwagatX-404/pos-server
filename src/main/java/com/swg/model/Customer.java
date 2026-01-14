c87e0cfb8caf9cc1765acd56720cfe7e6ac9d6aa
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-14 13:40:48 +0530

    feat: Customer model

---
+++ src/main/java/com/swg/model/Customer.java
@@ -0,0 +1,31 @@
+package com.swg.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    private String email;
+
+    private String phone;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
