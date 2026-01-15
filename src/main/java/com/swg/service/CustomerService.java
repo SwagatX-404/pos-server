6fffee49a21a682622d345c9534ee0629da4450b
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-15 14:54:28 +0530

    fix: throws exception

--- src/main/java/com/swg/service/CustomerService.java
+++ src/main/java/com/swg/service/CustomerService.java
@@ -7,9 +7,9 @@ import java.util.List;
 public interface CustomerService {
 
     Customer createCustomer(Customer customer);
-    Customer updateCustomer(Long id, Customer customer);
-    void deleteCustomer(Long id);
-    Customer getCustomer(Long id);
+    Customer updateCustomer(Long id, Customer customer) throws Exception;
+    void deleteCustomer(Long id) throws Exception;
+    Customer getCustomer(Long id) throws Exception;
     List<Customer> getAllCustomers();
     List<Customer> searchCustomers(String keyword);
 }
