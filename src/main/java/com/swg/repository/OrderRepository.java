6706d5a36919759b191795ebd84b17de0a64e95d
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-16 14:20:21 +0530

    feat: OrderRepository

---
+++ src/main/java/com/swg/repository/OrderRepository.java
@@ -0,0 +1,20 @@
+package com.swg.repository;
+
+import com.swg.model.Order;
+import com.swg.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order>findByCustomerId(Long customerId);
+    List<Order>findByBranchId(Long branchId);
+    List<Order>findByCashierId(Long cashierId);
+    List<Order>findByBranchIdAndCreatedAtBetween(Long branchId, LocalDateTime from, LocalDateTime to);
+    List<Order>findByCashierAndCreatedAtBetween(User cashier, LocalDateTime from, LocalDateTime to);
+    List<Order>findByTop5ByBranchIdOrderByCreatedAtDesc(Long branchId);
+
+
+}
