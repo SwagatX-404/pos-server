72a648877c153889096ddf21b75d576296e3bb3b
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-19 22:05:07 +0530

    feat: implement createOrder methode

--- src/main/java/com/swg/service/impl/OrderServiceImpl.java
+++ src/main/java/com/swg/service/impl/OrderServiceImpl.java
@@ -2,16 +2,67 @@ package com.swg.service.impl;
 
 import com.swg.domain.OrderStatus;
 import com.swg.domain.PaymentType;
+import com.swg.model.*;
 import com.swg.payload.dto.OrderDTO;
+import com.swg.repository.OrderRepository;
+import com.swg.repository.ProductRepository;
 import com.swg.service.OrderService;
+import com.swg.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+    private final UserService userService;
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+
+
     @Override
     public OrderDTO createOrder(OrderDTO orderDTO) throws Exception {
+        User cashier = userService.getCurrentUser();
+
+        Branch branch = cashier.getBranch();
+            if(branch==null){
+                throw new Exception("Cashier is not assigned to any branch...!");
+            }
+
+            Order order = Order.builder()
+                    .branch(branch)
+                    .cashier(cashier)
+                    .customer(orderDTO.getCustomer())
+                    .paymentType(orderDTO.getPaymentType())
+                    .build();
+
+            List<OrderItem> orderItems=orderDTO.getItems().stream().map(
+                    itemDto -> {
+                        Product product = productRepository.findById(itemDto.getProductId())
+                                .orElseThrow(
+                                        () -> new EntityNotFoundException("Product not found..!")
+                                );
+                        return OrderItem.builder()
+                                .product(product)
+                                .quantity(itemDto.getQuantity())
+                                .price(product.getSelligPrice()* itemDto.getQuantity())
+                                .order(order)
+                                .build();
+                    }
+            ).toList();
+            double total = orderItems.stream().mapToDouble(OrderItem::getPrice).sum();
+            order.setTotalAmount(total);
+            order.setItems(orderItems);
+
+            Order savedOrder = orderRepository.save(order);
+
         return null;
     }
+
+
 
     @Override
     public OrderDTO getOrderById(Long id) throws Exception {
