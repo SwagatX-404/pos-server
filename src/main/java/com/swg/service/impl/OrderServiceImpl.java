c237bc1c5e199ace14d734d7cc615c90e737cf1b
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-21 10:00:40 +0530

    feat: implement order retrieval and deletion methods in OrderServiceImpl

--- src/main/java/com/swg/service/impl/OrderServiceImpl.java
+++ src/main/java/com/swg/service/impl/OrderServiceImpl.java
@@ -13,7 +13,11 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Service
 @RequiredArgsConstructor
@@ -67,36 +71,83 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderDTO getOrderById(Long id) throws Exception {
-        return null;
+        return orderRepository.findById(id)
+                .map(OrderMapper::toDTO)
+                .orElseThrow(
+                ()-> new Exception("Order not found by id..!"+id)
+        );
     }
 
     @Override
-    public List<OrderDTO> getOrdersByBranch(Long branchId, Long customerId, Long cashierId, PaymentType paymentType, OrderStatus status) throws Exception {
-        return List.of();
+    public List<OrderDTO> getOrdersByBranch(Long branchId,
+                                            Long customerId,
+                                            Long cashierId,
+                                            PaymentType paymentType,
+                                            OrderStatus status) throws Exception {
+        return orderRepository.findByBranchId(branchId).stream()
+                .filter(order -> customerId==null ||
+                        order.getCustomer()!=null &&
+                                order.getCustomer().getId().equals(customerId))
+                .filter(order -> cashierId==null ||
+                        order.getCashier()!=null &&
+                                order.getCashier().getId().equals(cashierId))
+                .filter(order -> paymentType==null ||
+                        order.getPaymentType()==paymentType)
+
+                .map(OrderMapper::toDTO).collect(Collectors.toList());
+
+
     }
 
     @Override
     public List<OrderDTO> getOrderByCashier(Long cashierId) {
-        return List.of();
+        return orderRepository.findById(cashierId).stream()
+                .map(OrderMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public void deleteOrder(Long id) throws Exception {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(
+                        ()-> new Exception("Order not found by id..!"+id)
+                );
+        orderRepository.delete(order);
 
     }
 
     @Override
     public List<OrderDTO> getTodayOrdersByBranch(Long branchId) throws Exception {
-        return List.of();
+
+        LocalDate today = LocalDate.now();
+        LocalDateTime start = today.atStartOfDay();
+        LocalDateTime end = today.plusDays(1).atStartOfDay();
+
+        return orderRepository.findByBranchIdAndCreatedAtBetween(
+                branchId, start, end
+        ).stream()
+                .map(OrderMapper::toDTO)
+                .collect(Collectors.toList()
+        );
     }
 
     @Override
     public List<OrderDTO> getOrdersByCustomerId(Long customerId) throws Exception {
-        return List.of();
+        return orderRepository.findByCustomerId(customerId)
+                .stream()
+                .map(OrderMapper::toDTO)
+                .collect(Collectors.toList()
+                );
     }
 
     @Override
     public List<OrderDTO> getTop5RecentOrdersByBranchId(Long branchId) throws Exception {
-        return List.of();
+        return orderRepository.findTop5ByBranchIdOrderByCreatedAtDesc(branchId)
+                .stream()
+                .map(OrderMapper::toDTO)
+                .collect(Collectors.toList()
+
+                );
+
     }
 }
