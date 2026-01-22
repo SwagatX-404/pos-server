85b26160d583a554bae28121e01ba21be6e528f6
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-22 22:44:29 +0530

    feat: RefundDTO

---
+++ src/main/java/com/swg/payload/dto/RefundDTO.java
@@ -0,0 +1,48 @@
+package com.swg.payload.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.swg.domain.PaymentType;
+import com.swg.model.Branch;
+import com.swg.model.Order;
+import com.swg.model.ShiftReport;
+import com.swg.model.User;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class RefundDTO {
+
+
+    private Long id;
+
+
+    private OrderDTO order;
+    private  Long orderId;
+
+    private  String reason;
+
+    private Double amount;
+
+
+    //private ShiftReport shiftReport;
+    private Long shiftReportId;
+
+
+    private UserDto cashier;
+    private String cashierName;
+
+
+    private BranchDTO branch;
+    private Long branchId;
+
+    private PaymentType paymentType;
+    private LocalDateTime createdAt;
+
+}
