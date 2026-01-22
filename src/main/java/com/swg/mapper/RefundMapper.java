7207bed1e9bc65f53420ce7cea3e6e2d2c333a8d
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-22 22:45:29 +0530

    feat: RefundMapper mapper

---
+++ src/main/java/com/swg/mapper/RefundMapper.java
@@ -0,0 +1,22 @@
+package com.swg.mapper;
+
+import com.swg.model.Refund;
+import com.swg.payload.dto.RefundDTO;
+
+public class RefundMapper {
+
+    public static RefundDTO toDTO(Refund refund) {
+
+        return RefundDTO.builder()
+                .id(refund.getId())
+                .orderId(refund.getOrder().getId())
+                .reason(refund.getReason())
+                .amount(refund.getAmount())
+                .amount(refund.getAmount())
+                .cashierName(refund.getCashier().getFullName())
+                .branchId(refund.getBranch().getId())
+                .shiftReportId(refund.getShiftReport().getId())
+                .createdAt(refund.getCreatedAt())
+                .build();
+    }
+}
