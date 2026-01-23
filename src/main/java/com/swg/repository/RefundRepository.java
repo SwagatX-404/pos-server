8baa7db1c5bc4ed05391e95350fa0ef6d00ff6f9
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-23 22:08:32 +0530

    feat: interface RefundRepository

---
+++ src/main/java/com/swg/repository/RefundRepository.java
@@ -0,0 +1,23 @@
+package com.swg.repository;
+
+import com.swg.model.Refund;
+import com.swg.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface RefundRepository extends JpaRepository<Refund, Long> {
+
+    List<Refund> findByCashierAndCreatedAtBetween(
+            User cashier,
+            LocalDateTime from,
+            LocalDateTime to
+    );
+
+    List<Refund> findByCashierId(Long id);
+    List<Refund> findByShiftReportId(Long id);
+    List<Refund> findByBranchId(Long id);
+
+
+}
