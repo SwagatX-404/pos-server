e01d2c33587ce25d5300ca06ef9d3fb7bd76a6de
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2026-01-26 13:57:38 +0530

    fix: annotation & refund api endpoint testing is done

--- src/main/java/com/swg/payload/dto/RefundDTO.java
+++ src/main/java/com/swg/payload/dto/RefundDTO.java
@@ -10,13 +10,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefundDTO {
 
 
